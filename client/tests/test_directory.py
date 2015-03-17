cde4d03d23e036e8a1ea124564893ca0150295a3
neduchal <neduchal@gmail.com>
2015-03-17 15:17:06 +0100

    test_directory - upravy

--- client/tests/test_directory.py
+++ client/tests/test_directory.py
@@ -18,20 +18,21 @@ class TestSAKoDirectoryClass(unittest.TestCase):
 
     def setUp(self):
         pass
-        
+
     def test_find(self):
         dir = None
+        ok = 0
         try:
             dir = SAKo.directory(path_to_script)
         except:
             self.assert_(False)
         for i in range(dir.getCount()):
             filename = dir.getNext()
-            if filename is not None :
-                if filename == "test_directory.py":
-                    self.assert_(True)
-                    pass
-        self.assert_(False)
+            if filename is not None:
+                print os.path.basename(filename)
+                if os.path.basename(filename) == "test_directory.py":
+                    ok = 1
+        self.assert_(ok == 1)
 
     def test_previous(self):
         try:
@@ -40,33 +41,31 @@ class TestSAKoDirectoryClass(unittest.TestCase):
             self.assert_(False)
         dir.position = dir.getCount() - 1
         dir.getPrevious()
-        self.assert_(dir.position ==  dir.getCount() - 2)
-        
+        self.assert_(dir.position == dir.getCount() - 2)
+
     def test_next(self):
         try:
             dir = SAKo.directory(path_to_script)
         except:
             self.assert_(False)
         dir.getNext()
-        self.assert_(dir.position ==  1)
-        
+        self.assert_(dir.position == 1)
+
     def test_first(self):
         try:
             dir = SAKo.directory(path_to_script)
         except:
             self.assert_(False)
         filename = dir.getFirst()
-        self.assert_(dir.filelist[0] ==  filename)
-        
+        self.assert_(dir.filelist[0] == filename)
+
     def test_Last(self):
         try:
             dir = SAKo.directory(path_to_script)
         except:
             self.assert_(False)
         filename = dir.getLast()
-        self.assert_(dir.filelist[dir.getCount() - 1] ==  filename)
-        
-
+        self.assert_(dir.filelist[dir.getCount() - 1] == filename)
 
 if __name__ == "__main__":
     unittest.main()
