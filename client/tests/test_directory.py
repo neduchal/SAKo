08ffccbc0e85fd97ad570bc101c879032e688e3f
neduchal <neduchal@gmail.com>
2015-03-17 14:48:46 +0100

    nosetests

--- client/tests/test_directory.py
+++ client/tests/test_directory.py
@@ -6,6 +6,7 @@ __date__ = "$17.3.2015 13:37:46$"
 
 import unittest
 import os.path
+import sys
 
 path_to_script = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(path_to_script, "../"))
@@ -16,18 +17,21 @@ import SAKo
 class TestSAKoDirectoryClass(unittest.TestCase):
 
     def setUp(self):
-        self.dir = SAKo.directory("./")
+        self.dir = SAKo.directory(path_to_script)
 
     def test_getFirst(self):
         filename = self.dir.getFirst()
-        self.assert_(os.path.basename(filename) == "test.py")
+        print os.path.basename(filename)
+        self.assert_(os.path.basename(filename) == "test_directory.py")
 
     def test_getLast(self):
         filename = self.dir.getLast()
-        self.assert_(os.path.basename(filename) == "test.py")
+        print os.path.basename(filename)
+        self.assert_(os.path.basename(filename) == "test_directory.py")
 
     def test_getCount(self):
         count = self.dir.getCount()
+        print count
         self.assert_(count == 1)
 
 if __name__ == "__main__":
