40ff215938d1d4275658e5c5cc4a808f15f7660c
Petr Neduchal <neduchal@gmail.com>
2015-04-26 18:46:55 +0200

    Prvni nastrel verze 1.1

--- client/SAKo.py
+++ client/SAKo.py
@@ -12,6 +12,7 @@ Závislosti
 * os
 * os.path
 * urllib
+* glob
 
 **Ukázka použití klienta**
 
@@ -46,87 +47,24 @@ Do něj pak vložte následující kód.
 
 """
 
-# Nacteni knihoven
+# Imports
 import os
 import os.path
 import urllib
+import glob
+import argparse
 
-version = 1 * 1000000 + 0 * 1000 + 0 * 1
-
-
-class directory:
-    """
-        Třída pro práci se složkou
-    """
-    # construktor
-    def __init__(self, dirname):
-        """Konstruktor třídy
-
-           :param dirname: Cesta ke složce.
-           :type dirname: str.
-        """
-        self.filelist = []
-        for root, directories, files in os.walk(dirname):
-            for filename in files:
-                filepath = os.path.join(root, filename)
-                self.filelist.append(filepath)
-        self.filelist.sort()
-        self.position = 0
-        self.count = len(self.filelist)
-
-    def getNext(self):
-        """
-            Vrátí další položku v otevřené složce.
-
-            :returns: str -- cesta k souboru
-        """
-        self.position = self.position + 1
-        return self.filelist[self.position - 1]
-
-    def getPrevious(self):
-        """
-            Vrátí předchozí položku v otevřené složce.
-
-            :returns: str -- cesta k souboru
-        """
-        self.position = self.position - 1
-        return self.filelist[self.position + 1]
-
-    def getFirst(self):
-        """
-            Vrátí první položku v otevřené složce.
-
-            :returns: str -- cesta k souboru
-        """
-        return self.filelist[0]
-
-    def getLast(self):
-        """
-            Vrátí poslední položku v otevřené složce.
-
-            :returns: str -- cesta k souboru
-        """
-        return self.filelist[self.count - 1]
-
-    def getCount(self):
-        """
-            Vrátí počet položek v otevřené složce.
-
-            :returns: int -- pocet položek
-        """
-        return self.count
-
-    pass
+version = 1 * 1000000 + 1 * 1000 + 0 * 1
 
 
-def submit(app, dirname, login, passwd, task):
+def submit(app, dir_name, login, passwd, task):
     """Funkce pro odevzdání úlohy na server.
 
        :param app: Aplikace do které je kód odevzdáván.
        :type app: str.
 
-       :param dirname:  Složka s odevzdávanými soubory.
-       :type dirname: str.
+       :param dir_name:  Složka s odevzdávanými soubory.
+       :type dir_name: str.
        :param login:  Login do systému SAKo.
        :type login: str.
        :param passwd:  Heslo do systému SAKo.
@@ -135,18 +73,15 @@ def submit(app, dirname, login, passwd, task):
        :type task: str.
 
     """
-    url = "http://147.228.124.51/" + app + "/"
-
-    dir = directory(dirname)
-    data = {}
-    data['login'] = login
-    data['password'] = passwd
-    data['task'] = task
-    data['version'] = version
-
-    for i in range(dir.getCount()):
-        filename = dir.getNext()
-        print "Oteviram soubor : " + filename
+    f = None
+    url = 'http://147.228.124.51/' + app + '/'
+    if dir_name[-1:] == '/':
+        dir_name = dir_name[:-1]
+    files = glob.glob(dir_name + '/*.*')
+    data = dict(login=login, password=passwd, task=task, version=version)
+    for i in range(len(files)):
+        filename = files[i]
+        print 'Oteviram soubor : ' + filename
         f = open(filename, 'rb')
         filebody = f.read()
         data['name' + str(i)] = filename
@@ -154,12 +89,28 @@ def submit(app, dirname, login, passwd, task):
     print "Komunikace se serverem..."
     u = urllib.urlopen(url, urllib.urlencode(data))
     respond = u.read()
-    respond_arr = respond.split('##')
-    if (respond_arr[0] == 'actualize'):
-        data = urllib.urlretrieve(respond_arr[1], os.path.abspath(__file__))
+    if respond[0:9] == 'actualize':
+        urllib.urlretrieve(respond[11:], os.path.abspath(__file__))
         print "Klient byl aktualizovan. Provedte novy pokus o odevzdani"
     else:
         print "Vysledek :"
         print respond
     f.close()
     return respond
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--directory', 'Cesta k odevzdavanym souborum, napriklad ./src')
+    parser.add_argument('-l', '--login', 'Login do systemu')
+    parser.add_argument('-p', '--password', 'Heslo do systemu')
+    parser.add_argument('-a', '--app', 'Zarazeni v systemu [zdo, mpv, ...]')
+    parser.add_argument('-t', '--task', 'Nazev odevzdavane ulohy')
+
+    argv = parser.parse_args()
+
+    submit(argv.app, argv.directory, argv.login, argv.password, argv.task)
+
+
+
+
