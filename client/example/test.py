c747472d41876e890bcc93c1c7d9615bf5ccef81
neduchal <neduchal@gmail.com>
2015-04-26 19:01:37 +0200

    Uprava test.py

--- client/example/test.py
+++ client/example/test.py
@@ -11,4 +11,4 @@ sys.path.append(os.path.join(path_to_script, "../"))
 import SAKo
 
 # SAKo.submit(ZDO/MPV, SLOZKA, LOGIN, PASSWORD, TASK)
-SAKo.submit("zdo", "./src/", "test", "test", 'copy')
+SAKo.submit("zdo", "./", "test", 'test', 'copy')
