b9d9af9aec27a02a090fc768ce548f02d9f54ce2
Petr Neduchal <neduchal@gmail.com>
2015-10-07 13:53:12 +0200

    verze 1.1.0

--- client/SAKo.py
+++ client/SAKo.py
@@ -52,19 +52,14 @@ import os
 import os.path
 import urllib
 import glob
-import argparse
-import pickle
 
 version = 1 * 1000000 + 1 * 1000 + 0 * 1
 path_to_script = os.path.dirname(os.path.abspath(__file__))
 
 
-def submit(p_app, p_dir_name, p_login, p_passwd, p_task):
+def submit(p_dir_name, p_login, p_passwd, p_task):
     """
         Funkce pro odevzdání úlohy na server.
-
-        :param p_app: Aplikace do které je kód odevzdáván.
-        :type p_app: str.
 
         :param p_dir_name:  Složka s odevzdávanými soubory.
         :type p_dir_name: str.
@@ -78,89 +73,41 @@ def submit(p_app, p_dir_name, p_login, p_passwd, p_task):
 
     """
     p_f = None
-    url = 'http://147.228.124.51/' + p_app + '/'
+    url = 'http://147.228.124.51/sako/'
     if p_dir_name[-1:] == '/':
         p_dir_name = p_dir_name[:-1]
     files = glob.glob(p_dir_name + '/*.*')
+
     p_data = dict(login=p_login, password=p_passwd,
-                  task=p_task, version=version)
-    for i in range(len(files)):
-        filename = files[i]
-        print 'Oteviram soubor : ' + filename
-        p_f = open(filename, 'rb')
-        file_data = p_f.read()
-        p_data['name' + str(i)] = filename
-        p_data['file' + str(i)] = file_data
-    print 'Komunikace se serverem...'
+                  task=p_task, version=version, ticket='')
     u = urllib.urlopen(url, urllib.urlencode(p_data))
-    respond = u.read()
-    if respond[0:9] == 'actualize':
+    respond = u.read().strip()
+
+    if respond[0:3] == '0##':
+        print 'Nepodarilo se ziskat listek'
+    elif len(respond) == 0:
+        print 'Server neodpovida'
+    elif respond[0:9] == 'actualize':
         urllib.urlretrieve(respond[11:], os.path.abspath(__file__))
         print 'Klient byl aktualizovan. Provedte novy pokus o odevzdani'
     else:
-        print "Vysledek :"
-        print respond
-    if p_f is not None:
-        p_f.close()
-    return respond
-
-
-def create_identification_file(identity):
-    """
-        Interaktivni vytvoreni identifikacniho souboru
-    """
-    p_login = raw_input('Zadejte vase uzivatelske jmeno: ')
-    p_password = raw_input('Zadejte vase heslo: ')
-    p_app = raw_input('Vase zarazeni [zdo, mpv, ...]: ')
-
-    try:
-        p_f = open(path_to_script + '/' + identity + '.pck', 'wb')
-        p_data = dict(login=p_login, password=p_password, app=p_app)
-        pickle.dump(p_data, p_f, pickle.HIGHEST_PROTOCOL)
-        p_f.close()
-    except IOError as e:
-        print 'I/O error ({0}): {1}'.format(e.errno, e.strerror)
-        return 0
-    print 'Soubor', path_to_script + '/identity.pck', 'byl vytvoren'
-    return 1
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--directory',
-                        help='Cesta k odevzdavanym souborum [./src]')
-    parser.add_argument('-l', '--login',
-                        help='Login do systemu')
-    parser.add_argument('-p', '--password',
-                        help='Heslo do systemu')
-    parser.add_argument('-a', '--app',
-                        help='Zarazeni v systemu [zdo, mpv, ...]')
-    parser.add_argument('-t', '--task',
-                        help='Nazev odevzdavane ulohy')
-    parser.add_argument('-c', '--create',
-                        help='Vytvoreni identifikacniho souboru [only|yes]',
-                        default='none')
-    parser.add_argument('-i', '--identity', help='Soubor s identitou',
-                        default='identity')
-
-    argv = parser.parse_args()
-
-    if argv.create != 'none':
-        create_identification_file(argv.identity)
-
-    if argv.create != 'only':
-        task = argv.task
-        directory = argv.directory
-        a_id = argv.identity
-        print path_to_script + '/' + a_id + '.pck'
-        if os.path.isfile(path_to_script + '/' + a_id + '.pck'):
-            with open(path_to_script + '/' + a_id + '.pck', 'rb') as f:
-                data = pickle.load(f)
-                login = data['login']
-                password = data['password']
-                app = data['app']
+        p_data['ticket'] = respond
+        for i in range(len(files)):
+            filename = files[i]
+            print 'Oteviram soubor : ' + filename
+            p_f = open(filename, 'rb')
+            file_data = p_f.read()
+            p_data['name' + str(i)] = filename
+            p_data['file' + str(i)] = file_data
+        print 'Komunikace se serverem...'
+        u = urllib.urlopen(url, urllib.urlencode(p_data))
+        respond = u.read().strip()
+        if respond[0:3] == '0##':
+            print "Chyba : "
+            print respond[3:]
         else:
-            login = argv.login
-            password = argv.password
-            app = argv.app
-        submit(app, directory, login, password, task)
+            print "Vysledek :"
+            print respond
+        if p_f is not None:
+            p_f.close()
+    return respond
